5644e6b73b01465eed1070009902ce430b63e04a
Ogtovar <ogtovar@gmail.com>
2024-02-08 16:23:00 +0000

    Updating

---
+++ challenge.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#If tony entered meme response will execute
+
+name = input("What is your name?")
+
+day = input("What day of the week is it?")
+
+if name == "Tony":
+    print("Well F U Tony!")
+
+else:
+    print("Hello,"+ name + "! Happy"+ day+"!")
